764c5fb549fb228efd30f60a69e2555acd79df93
Marius Darila <marius.darila@gmail.com>
2022-03-07 13:33:10 +0100

    chore(): cleanup again

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/mariusdarila/go/src/akasha-docs/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/mariusdarila/go/src/akasha-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/mariusdarila/go/src/akasha-docs/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
-  require('/Users/mariusdarila/go/src/akasha-docs/src/css/custom.css'),
-];
