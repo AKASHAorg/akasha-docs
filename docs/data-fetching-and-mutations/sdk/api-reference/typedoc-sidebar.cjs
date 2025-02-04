ad984ef50541733fd0e83ba7d8981b2e4b3d3ce6
Josemaria Nriagu <josemaria.nriagu@akasha.world>
2025-02-04 10:21:44 +0100

    chore: update gitignore

--- docs/data-fetching-and-mutations/sdk/api-reference/typedoc-sidebar.cjs
+++
@@ -1,4 +0,0 @@
-// @ts-check
-/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
-const typedocSidebar = { items: [{"type":"category","label":"Namespaces","items":[{"type":"doc","id":"data-fetching-and-mutations/sdk/api-reference/Namespace.typings","label":"typings"}]}]};
-module.exports = typedocSidebar.items;
